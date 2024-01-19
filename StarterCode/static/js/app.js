cd1c88a2fed94006afc295348ee0554d688fbf8d
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-01-18 23:04:16 -0600

    cleanup & comments

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,26 +4,32 @@
 let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
 // Function for Bar Chart init
-function createBarChart(id) {
-    const chartData = sampleData[id];
+function createBarChart(chartData) {
     console.log(chartData);
-    let topNum = 10;
     let plotData = [{
-        x: chartData.sample_values.slice(0,topNum).reverse(),
-        y: chartData.otu_ids.map(x => 'OTU ' + x).slice(0,topNum).reverse(),
-        text: chartData.otu_labels.map(x => x.replace(/;/g, '<br>')).slice(0,topNum).reverse(),
+        x: chartData.sample_values,
+        y: chartData.otu_ids.map(x => 'OTU ' + x),
+        text: chartData.otu_labels.map(x => x.replace(/;/g, '<br>')),
         type: 'bar',
         orientation: 'h'
-      }];
+    }];
 
-      console.log(plotData);
+    console.log(plotData);
 
-      let layout = {
-        title: `ID ${id} Sample Data`
-      };
+    let layout = {
+        title: `ID ${chartData.id} Sample Data`,
+        xaxis: { title: 'Count' },
+        yaxis: { title: 'OTU IDs' }
+    };
 
-      Plotly.newPlot('bar', plotData, layout);
+    Plotly.newPlot('bar', plotData, layout);
 
+};
+
+// When selector is changed, populate the chart.
+function optionChanged(id) {
+    const chartData = sampleData[id];
+    createBarChart(chartData);
 };
 
 // Create a DataPromise to load JSON async
@@ -32,15 +38,24 @@ let dataPromise = d3.json(url);
 
 const sampleData = {}
 
-dataPromise.then(function(data) {
-   // console.log(data);
-    data.names.map(k => {selector.append('option').attr('value', k).text(k)});
-    data.samples.forEach(entry => {
-        sampleData[entry.id] = entry;
-    });
-    //console.log(sampleData);
-}); 
+dataPromise.then(function (data) {
+    // Append the ID names to the Selector
+    console.log(data.samples[0]);
 
-function optionChanged(id) {
-    createBarChart(id);
-} 
+    data.names.map(k => { selector.append('option').attr('value', k).text(k) });
+
+    //Create a keyed Samples dictionary for lookup later - slicing top 10 samples
+    let topNum = 10;
+
+    data.samples.forEach(entry => {
+        sampleData[entry.id] = {
+            id: entry.id,
+            sample_values: entry.sample_values.slice(0, topNum).reverse(),
+            otu_ids: entry.otu_ids.slice(0, topNum).reverse(),
+            otu_labels: entry.otu_labels.slice(0,topNum).reverse(),
+        }
+    });
+
+    //init the charts with first record
+    optionChanged(data.names[0]);
+}); 
