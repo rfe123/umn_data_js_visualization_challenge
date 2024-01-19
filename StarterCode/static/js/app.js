ddb1d690dc221c73d5cae94409b8cf7ab5e183a6
rfe123 <59402267+rfe123@users.noreply.github.com>
2024-01-18 22:53:11 -0600

    implement selector and bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,46 @@
+// const { layerGroup } = require("leaflet");
+
 // Define the URL to load sample data
 let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
 // Function for Bar Chart init
-function createBarChart(sampleData) {
-    console.log(sampleData);
+function createBarChart(id) {
+    const chartData = sampleData[id];
+    console.log(chartData);
+    let topNum = 10;
+    let plotData = [{
+        x: chartData.sample_values.slice(0,topNum).reverse(),
+        y: chartData.otu_ids.map(x => 'OTU ' + x).slice(0,topNum).reverse(),
+        text: chartData.otu_labels.map(x => x.replace(/;/g, '<br>')).slice(0,topNum).reverse(),
+        type: 'bar',
+        orientation: 'h'
+      }];
+
+      console.log(plotData);
+
+      let layout = {
+        title: `ID ${id} Sample Data`
+      };
+
+      Plotly.newPlot('bar', plotData, layout);
+
 };
 
 // Create a DataPromise to load JSON async
+let selector = d3.select('#selDataset');
 let dataPromise = d3.json(url);
 
-dataPromise.then(function(data) {
-    console.log(data);
+const sampleData = {}
 
-    createBarChart(data.samples);
-});
+dataPromise.then(function(data) {
+   // console.log(data);
+    data.names.map(k => {selector.append('option').attr('value', k).text(k)});
+    data.samples.forEach(entry => {
+        sampleData[entry.id] = entry;
+    });
+    //console.log(sampleData);
+}); 
+
+function optionChanged(id) {
+    createBarChart(id);
+} 
